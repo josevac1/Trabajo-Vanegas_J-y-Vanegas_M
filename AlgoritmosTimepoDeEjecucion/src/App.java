8751c0443f4dcf73410c4a2ad885b543024cd9cc
jose vanegas <josevanp05@gmail.com>
2024-05-18 23:43:38 -0500

    Guardado

--- AlgoritmosTimepoDeEjecucion/src/App.java
+++ AlgoritmosTimepoDeEjecucion/src/App.java
@@ -6,9 +6,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         MetodosDeOrdenamiemto metodo = new MetodosDeOrdenamiemto();
         Random random = new Random();
-        int[] sizes = {10,20, 50, 100, 1000, 5000, 10000,30000};
+        int[] tamano = {10,20, 50, 100, 1000, 5000, 10000,30000};
 
-        for (int s : sizes) {
+        for (int s : tamano) {
             int[] lista = new int[s];
             for (int i = 0; i < s; i++) {
                 lista[i] = random.nextInt();
